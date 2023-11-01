6884bd75323492ed31d12738b54bac514bc08e02
Marcus Thorne <marcus_thorne475@hotmail.com>
2023-11-01 11:29:57 +0000

    s

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ async function startServer() {
     console.log(`Server listening on PORT ${PORT}`);
   })
 
-  app.use(express.static(path.join(__dirname, 'client/public')));
+  app.use(express.static(path.join(__dirname, './client/public')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/public/index.html'))
+    res.sendFile(path.join(__dirname + './client/public/index.html'))
   });
 }
 
